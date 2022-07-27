2a058002e77c2070bb8ce164ec08a279bfbd55e5
Tharath Tho <tharath@sala.co>
2022-07-27 13:36:41 +0700

    initial commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+    messagesService: MessagesService;
+
+    constructor() {
+        this.messagesService = new MessagesService();
+    }
+    @Get()
+    listMessages() {
+        return this.messagesService.findAll();
+    }
+
+    @Post()
+    createMessages(@Body() body: CreateMessageDto) {
+        return this.messagesService.create(body.content)
+    }
+
+    @Get('/:id')
+    getMessage(@Param('id') id: string) {
+        const message = this.messagesService.findOne(id)
+        if (!message) {
+            throw new NotFoundException('Message not found.')
+        }
+        return message;
+    }
+}
